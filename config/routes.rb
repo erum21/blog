3bfbc500037c305ac44675a04bba900ee7d7aab8
Edwin Vega <edwin.ponc@gmail.com>
2010-09-14 22:54:00 -0400

    Added a Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Blog::Application.routes.draw do
+
+
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/about"
+
+  get "pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -56,3 +66,4 @@ Blog::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 end
+
