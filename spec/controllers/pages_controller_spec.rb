715e821ff1091622d14e3ce48de141ce28b01e56
Edwin Vega <edwin.ponc@gmail.com>
2010-09-14 22:54:56 -0400

    Updated some files

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe PagesController do
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'helpp'" do
+    it "should be successful" do
+      get 'helpp'
+      response.should be_success
+    end
+  end
+
+end
